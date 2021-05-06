12b1e74661e5e0a1c155cb19da6825b956f0088a
hscannell <scannell@ldeo.columbia.edu>
2021-05-06 09:04:39 -0600

    fixed filter_area test

--- tests/test_track.py
+++ tests/test_track.py
@@ -15,7 +15,6 @@ from skimage.measure import regionprops
 from skimage.measure import label as label_np
 import dask.array as dsa
 
-
 def example_anomaly_data():
     x0 = [180, 225, 360, 80, 1, 360, 1]
     y0 = [0, 20, -50, 40, -50, 40, 40]
@@ -75,16 +74,27 @@ def test_apply_mask():
     Anom, mask = example_anomaly_data()
     Anom_dask = Anom.chunk({'time': 1})
     binary_images = _morphological_operations(Anom_dask, radius=8)
-    binary_images_with_mask = binary_images.where(mask==1, drop=False, other=0)
-    
+    binary_images_with_mask = _apply_mask(mask, binary_images)    
     assert (binary_images_with_mask.where(mask==0, drop=True)==0).all()
     
+def test_filter_area():
+    Anom, mask = example_anomaly_data()
+    Anom_dask = Anom.chunk({'time': 1})
+    binary_images = _morphological_operations(Anom_dask, radius=8)
+    binary_images_with_mask = _apply_mask(mask, binary_images)
+
+    min_size_quartile = .75
+    area, min_area, binary_labels, N_initial = _filter_area(binary_images_with_mask, min_size_quartile)
+    
+    assert min_area == 2761.5
+    assert N_initial.astype(int) == 15
+
 
 def test_label_either():
     Anom, mask = example_anomaly_data()
     Anom_dask = Anom.chunk({'time': 1})
     binary_images = _morphological_operations(Anom_dask, radius=8)
-    binary_images_with_mask = binary_images.where(mask==1, drop=False, other=0)
+    binary_images_with_mask = _apply_mask(mask, binary_images)
 
     labels, num = _label_either(binary_images_with_mask, return_num= True, connectivity=3)
     
@@ -92,48 +102,26 @@ def test_label_either():
     assert all([i in labels[2,:,:] for i in range(0,6)])
     
 
-def test_filter_area():
-    Anom, mask = example_anomaly_data()
-    Anom_dask = Anom.chunk({'time': 1})
-    binary_images = _morphological_operations(Anom_dask, radius=8)
-    binary_images_with_mask = binary_images.where(mask==1, drop=False, other=0)
-    labels, num = _label_either(binary_images_with_mask, return_num= True, connectivity=3)
-    
-    min_size_quartile = .75
-    area, min_area, labels_greater_minsize, labelprops = _filter_area(labels, min_size_quartile)
-    
-    assert min_area == 3413.0
-    assert all([i in labels_greater_minsize for i in [1,4]])
-    assert (area.label[area>=min_area].values == np.unique(labels_greater_minsize[labels_greater_minsize>0])).all()
-    
+
     
 def test_wrap():
     Anom, mask = example_anomaly_data()
     Anom_dask = Anom.chunk({'time': 1})
     binary_images = _morphological_operations(Anom_dask, radius=8)
-    binary_images_with_mask = binary_images.where(mask==1, drop=False, other=0)
-    labels, num = _label_either(binary_images_with_mask, return_num= True, connectivity=3)
+    binary_images_with_mask = _apply_mask(mask, binary_images)
     min_size_quartile = .75
-    area, min_area, labels_greater_minsize, labelprops = _filter_area(labels, min_size_quartile)
     
-    out_labels, N = _wrap(labels_greater_minsize)
-    
-    assert np.max(out_labels) == 2
+    area, min_area, binary_labels, N_initial = _filter_area(binary_images_with_mask, min_size_quartile)    
+    labels, num = _label_either(binary_images_with_mask, return_num= True, connectivity=3)
+    labels_wrapped, N_final = _wrap(labels)
+
+    assert N_final == 4
 
 
 @pytest.mark.parametrize("radius", [8, 10])
-@pytest.mark.parametrize("area_quantile", [0.75, 0.80])
+@pytest.mark.parametrize("min_size_quartile", [0.75, 0.80])
 def test_track(radius, area_quantile):
     Anom, mask = example_anomaly_data()
     Anom_dask = Anom.chunk({'time': 1})
-    new_labels = track(Anom, mask, radius=radius, area_quantile=area_quantile)
-    
-    assert (new_labels.percent_area_reject + new_labels.percent_area_accept) == 1.0
-
-    props = regionprops(new_labels.astype('int'))
-    labelprops = [p.label for p in props]
-    labelprops = xr.DataArray(labelprops, dims=['label'], coords={'label': labelprops}) 
-    area = xr.DataArray([p.area for p in props], dims=['label'], coords={'label': labelprops}) 
-    
-    assert area.min() >= new_labels.min_area
-    
+    new_labels = track(Anom, mask, radius=radius, min_size_quartile=min_size_quartile)
+    assert (new_labels.attrs['percent area reject'] + new_labels.attrs['percent area accept']) == 1.0

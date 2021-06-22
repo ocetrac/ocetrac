720f1186d689312ea79dd2a91d7e0849f9cf88f2
Hillary Scannell <scannell@ldeo.columbia.edu>
2021-06-22 14:55:58 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,16 @@ exclude_patterns = ["_build", "**.ipynb_checkpoints", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster" #"pangeo"
+html_theme = "sphinx_rtd_theme" #"pangeo"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+    "style_nav_header_background": "#343131",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

6032388df752995833e7642b54aefb0484e56969
hscannell <scannell@ldeo.columbia.edu>
2021-10-22 16:38:28 -0400

    create dataframe

--- ocetrac/metrics.py
+++ ocetrac/metrics.py
@@ -1,8 +1,74 @@
+import os, glob
 import xarray as xr
 import numpy as np
 import pandas as pd
 
-def to_dataframe(event, ssta):
+def _wrap(labels):
+    ''' Impose periodic boundary and wrap labels, then reorder the labels'''
+    first_column = labels[..., 0]
+    last_column = labels[..., -1]
+
+    stacked = first_column.stack(z=['time','lat'])
+    unique_first = np.unique(stacked[stacked.notnull()])
+
+    # This loop iterates over the unique values in the first column, finds the location of those values in 
+    # the first columnm and then uses that index to replace the values in the last column with the first column value
+    for i in enumerate(unique_first):
+        first = np.where(first_column == i[1])
+        last = last_column[first[0], first[1]]
+        stacked = last.stack(z=['time','lat'])
+        bad_labels = np.unique(stacked[stacked.notnull()])
+        replace = np.isin(labels, bad_labels)
+        labels = labels.where(replace==False, other=i[1])
+    
+    labels = labels.fillna(0)
+    labels_wrapped = np.unique(labels, return_inverse=True)[1].reshape(labels.shape)
+    labels_wrapped = xr.DataArray(labels_wrapped, dims=labels.dims, coords=labels.coords)
+
+    return labels_wrapped
+
+def _get_labels(binary_images):
+    '''function used to label binary images at each time step using skimage.measure.label'''
+    blobs_labels = label_np(binary_images, background=0)
+    return blobs_labels
+    
+def _get_centroids(sub_labels):
+    '''This function uses skimage.measure.regionprops to find the centroids of objects assigned 
+    to each unique label'''
+    props = regionprops(sub_labels.astype('int'))
+    centroids = [(float(sub_labels.lat[round(p.centroid[0])].values),
+                  float(sub_labels.lon[round(p.centroid[1])].values)) for p in props]
+    for i in range(0,len(centroids)):
+        if centroids[i][1] >= 359.875:
+            centroids[i] = (centroids[i][0], list(centroids[i])[1] - 359.875)
+    
+    return centroids
+
+def _get_intensity_area(event, ssta, mhw):
+    '''Calculates event intensities and area at each time interval using anomaly data and coordinates 
+    cooresponding to the event.'''
+    
+    event_ssta = ssta.where(event>0, drop=True)        
+    mhw['intensity_mean'].append(event_ssta.mean(('lat','lon')).values)
+    mhw['intensity_max'].append(event_ssta.max(('lat','lon')).values) 
+    mhw['intensity_min'].append(event_ssta.min(('lat','lon')).values)
+    mhw['intensity_cumulative'].append(np.nansum(event_ssta))
+    coords = event.stack(z=('lat','lon'))
+    coord_pairs = [(coords.isel(time=t[0]).dropna(dim='z', how='any').z.lat.values, 
+                      coords.isel(time=t[0]).dropna(dim='z', how='any').z.lon.values) for t in enumerate(event.time)]
+
+    mhw['coords'].append(coord_pairs)
+
+    # Calculate weighted cell area assuming 0.25º resolution data and 111 km per degree of latitude
+    y, x = zip(*coord_pairs)
+    dlon = [np.cos(y[c]*np.pi/180)*(111*.25) for c in np.arange(0, len(coord_pairs))]
+    dlat = (111*.25) * np.ones(len(dlon))
+    cell_area = [np.sum(dlon[c]*dlat[c]) for c in np.arange(0, len(coord_pairs))]
+    mhw['area'].append(cell_area)
+
+    return mhw
+    
+def _save_csv(event, ssta):
     '''
     Creates a Pandas DataFrame of event attributes.
     
@@ -36,25 +102,25 @@ def to_dataframe(event, ssta):
     '''
     
     # Initialize dictionary 
-    metrics = {}
-    metrics['id'] = [] # event label
-    metrics['date'] = [] # datetime format
-    metrics['coords'] = [] # (lat, lon)
-    metrics['centroid'] = []  # (lat, lon)
-    metrics['duration'] = [] # [months]
-    metrics['intensity_max'] = [] # [deg C]
-    metrics['intensity_mean'] = [] # [deg C]
-    metrics['intensity_min'] = [] # [deg C]
-    metrics['intensity_cumulative'] = [] # [deg C]
-    metrics['area'] = [] # [km2]
+    mhw = {}
+    mhw['id'] = [] # event label
+    mhw['date'] = [] # datetime format
+    mhw['coords'] = [] # (lat, lon)
+    mhw['centroid'] = []  # (lat, lon)
+    mhw['duration'] = [] # [months]
+    mhw['intensity_max'] = [] # [deg C]
+    mhw['intensity_mean'] = [] # [deg C]
+    mhw['intensity_min'] = [] # [deg C]
+    mhw['intensity_cumulative'] = [] # [deg C]
+    mhw['area'] = [] # [km2]
 
     # TO ADD:
     # mhw['rate_onset'] = [] # [deg C / month]
     # mhw['rate_decline'] = [] # [deg C / month]
 
-    metrics['id'].append(int(np.nanmedian(event.values)))
-    metrics['date'].append(event.time.values.astype('datetime64[M]'))
-    metrics['duration'].append(event.time.shape[0])
+    mhw['id'].append(int(np.nanmedian(event.values)))
+    mhw['date'].append(event.time.values.astype('datetime64[M]'))
+    mhw['duration'].append(event.time.shape[0])
 
     # Turn images into binary
     binary_event = event.where(event>=0, other=0)
@@ -86,8 +152,37 @@ def to_dataframe(event, ssta):
         east['lon'] = np.arange(360.125, 540.125, .25)
         append_east = xr.concat([lx.where(lx.lon >= 180, drop=True), east], dim="lon")
         centroid.append(_get_centroids(append_east))
-    metrics['centroid'].append(centroid)
+    mhw['centroid'].append(centroid)
     
-    metrics = pd.DataFrame(dict([(name, pd.Series(data)) for name,data in metrics.items()]))
+    mhw = pd.DataFrame(dict([(name, pd.Series(data)) for name,data in mhw.items()]))
+    mhw.to_csv('df_'+str(mhw['id'].values[0]).zfill(4)+'.csv', index=False)
+    return 
 
-    return mhw
+
+def to_dataframe(blobs, ssta):
+
+    directory = os.getcwd()+'/ocetrac_csv/'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    os.chdir(directory)
+    
+    num_events = int(np.nanmax(labels)) # number of events from Ocetrac
+    print(f'CSV files are saved here: {directory}')
+    for i in tqdm(range(1,13)):#num_events+1)):
+        event = labels.where(labels==i, drop=True).reindex({"lon": labels.lon.values})
+        _save_csv(event, ssta)
+    
+    csvfiles = glob.glob(os.path.join(directory, '*.csv'))
+    dataframes = []  
+    for csvfile in csvfiles:
+        df = pd.read_csv(csvfile)
+        dataframes.append(df)
+    
+    df = pd.concat(dataframes, ignore_index=True)
+    
+    for f in glob.glob(os.path.join(directory, '*.csv')):
+        if os.path.exists(f):
+            os.remove(f)
+    os.rmdir(directory)
+
+    return df

40d8083e719bc255c66f1b6821f2daf5b1d69a51
cohenjt <57732072+cohenjt@users.noreply.github.com>
2025-06-10 11:34:45 -0700

    Update authors.md and conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Ocetrac"
-copyright = "2025, Ocetrac"
-author = "Ocetrac"
+project = "ocetrac"
+copyright = "2025, ocetrac"
+author = "ocetrac"
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+# see https://pypi.org/project/setuptools-scm/ for details
+from pkg_resources import get_distribution
+
+
+release = get_distribution("ocetrac").version
+# for example take major/minor
+version = ".".join(release.split(".")[:2])
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -29,5 +40,33 @@ source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"  # "pangeo", "alabaster"
 html_static_path = ["_static"]
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+    "style_nav_header_background": "#343131",
+}
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "img/tranparent_logo.png"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = "ReadtheDocsTemplatedoc"
+
+# -- nbsphinx specific options ----------------------------------------------
+# this allows notebooks to be run even if they produce errors.
+nbsphinx_allow_errors = True

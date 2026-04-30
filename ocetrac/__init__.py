d53cf21dcb1dcef0bcfcc9f6cdc1ff47d7ac64d0
CassiaCai <fmc2855@uw.edu>
2026-04-30 16:49:23 -0600

    restructuring ocetrac

--- ocetrac/__init__.py
+++ ocetrac/__init__.py
@@ -1,2 +1,15 @@
-from . import measures, utils
-from .tracker import Tracker, _apply_mask
+# ============================================================
+# ocetrac/__init__.py
+# ============================================================
+"""
+ocetrac
+ 
+------------
+preprocessing : anomaly computation, morphological cleaning, thresholding
+DeepTrack     : 4-D connected-component labelling and temporal tracking
+              (time, z_t, nlat, nlon); tracks volumetric events
+              through depth and time
+SurfTrack     : 3-D connected-component labelling and temporal tracking
+              (time, nlat, nlon); tracks at one-depth level through
+               time only
+"""
